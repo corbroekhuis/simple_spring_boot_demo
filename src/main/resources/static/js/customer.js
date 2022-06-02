bcfdcf543e0332b0527b3cf14c8757a04314cc2b
Cor Broekhuis <cornelius.broekhuis@capgemini.com>
2022-06-02 14:18:11 +0200

    Thymeleaf initial commit

--- src/main/resources/static/js/customer.js
+++ src/main/resources/static/js/customer.js
@@ -1,13 +1,15 @@
 var api = "http://localhost:9090/api/customer" ;
 
-init();
-
 function init(){
 
+    console.log('inside init' );
+
+    // Add click event to button
     $("#create-customer").click(function (){
         createCustomer();
     });
 
+    // Get customers from backend and update DOM
     getCustomers();
 
 }
@@ -15,19 +17,35 @@ function init(){
 function getCustomers(){
 
     console.log('inside getCustomers' );
+    // http:/localhost:9090/api/customer
+    // json list of customers
     $.ajax({
         url: api,
         type: "get",
         dataType: "json",
         contentType: "application/json",
         success: function(customers, textStatus, jqXHR){
-
+            // process the data (json) and modify the DOM
+            var html =
+            '<table class="table">' +
+            '<tr>' +
+            '<th>Name</th>' +
+            '<th>Address</th>' +
+            '<th>Age</th>' +
+            '</tr>';
             $.each(customers, function(index, customer) {
+                html = html + '<tr>'+
+                '<td>'+ customer.name + '</td>' +
+                '<td>'+ customer.address + '</td>' +
+                '<td>'+ customer.age + '</td>' +
+                '</tr>'
                 console.log(customer.name + ' ' + customer.age + ' yrs');
             });
 
-            // process the data (json) and modify the DOM
-            console.log('status: ' + jqXHR.status + ', data: ' + customers[0].name);
+            html = html + '</table>';
+
+            $("#customers").html(html);
+
         },
 
         fail: function (error) {
@@ -41,5 +59,51 @@ function getCustomers(){
 function createCustomer(){
 
     console.log('inside createCustomer' );
+    // Put customer data from page in Javascript object
+    var customerData = {
+            name: $("#name").val(),
+            address: $("#address").val(),
+            age: $("#age").val(),
+    }
+
+    // Transform Javascript object to json
+    var customerJson = JSON.stringify(customerData);
+
+    console.log(customerJson);
+
+    $.ajax({
+        url: api,
+        type: "post",
+        data: customerJson,    // json for request body
+        dataType: "json",
+        contentType: "application/json",
+        success: function(customer, textStatus, jqXHR){
+          console.log(customer);
+          console.log(
+          customer.name + ' ' +
+          customer.address + ' ' +
+          customer.age + ' yrs');
+
+          // process the data (json) and modify the DOM
+          console.log('post status: ' + jqXHR.status + ', data: '
+          + customer.name + ' ' +
+          + customer.address + ' ' +
+          + customer.age + ' yrs');
+
+          // Clear fields in page
+          $("#name").val('');
+          $("#address").val('');
+          $("#age").val('');
+
+          // Refresh table data
+          getCustomers();
+
+        },
+
+        fail: function (error) {
+          console.log('Error: ' + error);
+        }
+
+    });
 
 }
